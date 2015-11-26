c514a38ef8c67a55bdadeb63b69e7a1822e820d3
Dawie van Lill <djvanlill@gmail.com>
2015-11-26 09:03:29 +0200

    Added some info on vectors

---
+++ dawie_code/ch2.R
@@ -0,0 +1,15 @@
+
+#This is a chapter about how you work with data. 
+
+#Data structures
+
+#Vectors
+#One dimensional arrays that can hold numeric data
+#Combine function c() is used to form the vector (concatenate)
+
+a <- c(1, 2, 5, 3, 6, -2, 4)
+b <- c("one", "two", "three")
+c <- c(TRUE, TRUE, TRUE, FALSE, TRUE, FALSE)
+
+#These are some examples of vectors
+
