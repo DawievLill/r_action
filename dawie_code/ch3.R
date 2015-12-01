5d4560c4ee86e3e53598ded202fc88740f119eed
Dawie van Lill <djvanlill@gmail.com>
2015-12-01 14:04:23 +0200

    Notes up to reference lines (3.4.3)

--- dawie_code/ch3.R
+++ dawie_code/ch3.R
@@ -98,6 +98,70 @@ par(opar)
 
 
 #Customizing!
+#Titles
+
+#The general layout is like the following
+title(main="main title", sub="subtitle",
+      xlab="x-axis label", ylab="y-axis label")
+
+#One example 
+title(main="My Title", col.main="red", 
+      sub="My Subtitle", col.sub="blue",
+      xlab="My X label", ylab="My Y label",
+      col.lab="green", cex.lab=0.75)
+
+#col.main = "red" -- gives us a red title
+#col.sub = "blue" -- gives a blue subtitle 
+#col.lab = "green" -- gives green x,y labels
+#cex.lab = 0.75 -- labels are 75% of default
+
+#Axes
+#You can create deafult axes with the axis() function
+
+#Example of custom axes
+
+x <- c(1:10)
+y <- x
+z <- 10/x
+opar <- par(no.readonly = TRUE)
+
+par(mar=c(5,4,4,8) + 0.1) #Increases the margin
+plot(x, y, type ="b",
+     pch = 21, col="red",
+     yaxt="n", lty=3, ann=FALSE) #Plot x and y, while supressing annotations
+
+lines(x, z, type="b", pch=22, col="blue", lty=2) #Adds x and 1/x lines
+
+#Draws axes
+#at -- shows where tick marks should be drawn
+#labels -- labels to be placed at tick marks
+#tck -- length of each tick mark
+
+axis(2, at=x, labels=x, col.axis="red", las =2)
+axis(4, at=z, labels=round(z, digits = 2),
+     col.axis="blue", las =2, cex.axis=0.7, tck=-0.01)
+
+#Add titles and text
+mtext("y=1/x", side=4, line=3, cex.lab=1, las=2, col = "blue")
+
+title("An example of creative axes",
+      xlab = "X vlaues", ylab = "Y=X")
+
+par(opar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
